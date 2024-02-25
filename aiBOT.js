1522dbfa19a67df6d2666e298a3dc3384a697a7c
glololes <127004112+glololes@users.noreply.github.com>
2024-02-24 23:02:03 -0500

    changed things with the bot

--- aiBOT.js
+++ aiBOT.js
@@ -2,10 +2,8 @@ function askQuestion() {
     var question = document.getElementById('input').value;
     if (question.trim() === '') return;
 
-    // Display user question in the chatbox
     appendMessage('You', question);
 
-    // Send question to backend server
     fetch('/ask', {
         method: 'POST',
         headers: {
@@ -15,7 +13,6 @@ function askQuestion() {
     })
     .then(response => response.json())
     .then(data => {
-        // Display answer from backend in the chatbox
         appendMessage('Bot', data.answer);
     })
     .catch(error => {
